996d15682691a2b5bc593f002da3eb90d9472c4a
Edgar A Guevara <edgarguevara33@gmail.com>
2022-11-16 15:05:48 -0500

    feat we add the middlerware to cors

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+
 const routerApi = require('./routes');
 const { logError, errorHandler, boomErrorHandler } = require('./middlerwares/errorsHandler');
 
@@ -6,6 +8,18 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+//app.use(cors);// in this way we can accept every domain.
+const whitelist = ['http://localhost', 'https://myapp.co']; //for example
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido'));
+    }
+  }
+}
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('From EAG Store');
